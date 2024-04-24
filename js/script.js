4168f9ab2e8493ea07fee331ecad1bc1db2dd337
gbxventura <gabriel.ventura20200@gmail.com>
2024-04-24 19:16:32 -0300

    atualizações feitas no tempo livre

--- js/script.js
+++ js/script.js
@@ -25,3 +25,50 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Código de efeito digitação
+document.addEventListener("DOMContentLoaded", function () {
+  const mainContent = document.getElementById("main-content");
+  const h2Text = "[< Oi, eu sou o Gabriel! >]";
+  const pText = "~ Front-End | FullStack Developer ~";
+  const typingSpeed = 150; // Velocidade de digitação (em milissegundos)
+  const pauseDuration = 1500; // Tempo de pausa entre as partes (em milissegundos)
+
+  // Função para digitar o texto em um elemento
+  function typeText(element, text, callback) {
+    let index = 0;
+    const intervalId = setInterval(function () {
+      if (index <= text.length) {
+        element.textContent = text.substring(0, index);
+        index++;
+      } else {
+        clearInterval(intervalId);
+        if (callback) {
+          callback();
+        }
+      }
+    }, typingSpeed);
+  }
+
+  // Função para exibir o texto do h2 e iniciar a digitação do p após o h2
+  function typeH2() {
+    const h2Element = mainContent.querySelector("h2");
+    typeText(h2Element, h2Text, typeP); // Chama typeP após concluir h2
+  }
+
+  // Função para exibir o texto do p
+  function typeP() {
+    const pElement = mainContent.querySelector("p");
+    typeText(pElement, pText); // Exibe todo o texto do p
+  }
+
+  // Oculta todos os elementos com classe 'typing-text' inicialmente
+  const typingElements = mainContent.querySelectorAll(".typing-text");
+  typingElements.forEach(function (element) {
+    element.textContent = ""; // Limpa o conteúdo inicial
+    element.style.display = "none";
+  });
+
+  // Exibe e inicia a digitação do h2
+  const h2Element = mainContent.querySelector("h2");
+  h2Element.style.display = "block";
+  typeH2();
+});
